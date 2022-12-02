4ac1698d5c9cc455d4d439d70c288beba5d98e8a
Matt Simon <info@boxadesign.co.uk>
2022-12-02 14:59:02 +0000

    Update content

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Matt Simon - Front End Engineer`,
+    siteTitleAlt: `Matt Simon - Front End Engineer`,
+    siteHeadline: `Matt Simon - Front End Engineer`,
+    siteUrl: `https://mattsimon.info`,
+    siteDescription: `Just about me and stuff`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Matt Simon`,
   },
   trailingSlash: `never`,
   plugins: [
